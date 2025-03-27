a3419cce4559ecdb9960577fc9cf2a0b3a14b461
dabin <db021129@naver.com>
2025-03-27 16:55:50 +0900

    feat: create UserServiceTest

---
+++ commenthat/src/test/java/com/cookiek/commenthat/service/UserServiceTest.java
@@ -0,0 +1,48 @@
+package com.cookiek.commenthat.service;
+
+import com.cookiek.commenthat.domain.User;
+import com.cookiek.commenthat.repository.UserRepository;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+@Transactional
+//@Rollback(value = false)
+@Rollback
+public class UserServiceTest {
+
+    @Autowired UserService userService;
+    @Autowired UserRepository userRepository;
+    @Autowired EntityManager em;
+
+    @Value("${youtube.api.key}")
+    private String api;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        User user = new User();
+        user.setName("dabin");
+        user.setEmail("db@naver.com");
+        user.setPassword("dabin1234");
+        user.setApi(api);
+
+        //when
+        Long savedId = userService.join(user);
+
+        //then
+        em.flush();
+        assertEquals(user, userRepository.findOne(savedId));
+
+    }
+
+
+}
